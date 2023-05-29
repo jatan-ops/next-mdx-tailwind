aa4afca650c182de48c4d6e8a10ac8bdfbcd4331
jatan-ops <jatan.shah90@gmail.com>
2023-05-29 13:51:35 +0530

    init

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import '../app/globals.css'
+
+export default function About() {
+  return (
+    <>
+      hey Joe - Jinal2!
+      <h1 className="text-3xl font-bold tracking-tight text-blue-900">Dashboard</h1>
+      <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+    </>
+  )
+}
